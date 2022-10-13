3e762f166df96529707f072a5918dca8efb575d7
Olamphzy <61218900+Olamphzy@users.noreply.github.com>
2022-10-13 19:15:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ======= Typng Animation ==== */
+var typed = new Typed(".typing", {
+    strings:["...", "Web Designer", "web Developer", "Software Engineering", "Graphic Designer", "Game Strategic", "Educator"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
